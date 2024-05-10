c1c8cfd8348b3d4cfbe00c1f7c9fc092312f5dd7
CJhang <a4213987@gmail.com>
2024-05-10 10:50:18 +0800

    [app] v0.0.1 finished app 標準功能基本上完成

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # faster-whisper
+
+使用faster-whisper套件, 將語音檔案轉成文字稿.
+
+## 環境設定
+
+### 新增虛擬機
+
+`py -m venv venv`
+
+### 啟用虛擬工作環境
+
+`.\venv\Scripts\activate`
+
+升級pip套件
+`py -m pip install --upgrade pip`
+
+列出所有已安裝套件
+`pip freeze`
+
+常見相依性套件安裝方法
+`pip install -r requirements.txt`
+
+相依性套件輸出
+`pip freeze > requirements.txt`
+
+更新你的虛擬環境中的套件, 也可以使用
+`pip install -r requirements.txt --upgrade`
+
+### 安裝 Whisper
+
+安裝 Whisper 已被包在requirements.txt當中
+不需要特別使用`pip install openai-whisper`
+
+## 使用方式
+
+### 使用 whisper
+
+編輯定義音訊檔案和目錄
+
+```
+audio_file = '錄製.m4a'
+mins = 10 # 單位:分鐘
+model_setting = "large" # 您可以選擇不同大小的模型，例如 "small", "medium", "large", "base"
+```
+
+後
+
+終端機 執行 `py .\App.py`
+
+---
+
+## 未來版本
+
+### 更新內文
+
+新增可辨識時間的內容段落
+
+
+
---
+++ app.py
@@ -0,0 +1,105 @@
+import logging
+from pydub import AudioSegment
+from faster_whisper import WhisperModel
+import os
+import shutil
+from datetime import datetime
+
+# 定義音訊檔案和目錄
+audio_file = '錄製.m4a'
+mins = 10  # 單位：分鐘
+model_size = "large"  # 您可以選擇不同大小的模型，例如 tiny, base, small, medium, large, large-v2, large-v3
+device = "cpu"  # "cuda" 或 "cpu"
+compute_type = "float32"  # "float16" 或 "float32"
+mode = "subtitle"  # 'normal', 'timeline', 'subtitle'
+
+
+# 設定日誌記錄
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# 檢查並創建目錄的函數
+def ensure_directory_exists(directory_name):
+    """
+    檢查資料夾
+    """
+    try:
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name, exist_ok=True)
+            logging.info(f"已創建目錄：{directory_name}")
+        else:
+            logging.info(f"目錄已存在：{directory_name}")
+    except Exception as e:
+        logging.error(f"創建目錄 {directory_name} 時出錯：{e}")
+        raise
+
+def clean_up_chunks(directory):
+    """
+    移除目錄
+    """
+    try:
+        shutil.rmtree(directory)
+        logging.info(f"已刪除目錄：{directory}")
+    except Exception as e:
+        logging.error(f"刪除目錄 {directory} 時出錯：{e}")
+
+
+# 創建目錄
+audio_files_directory = 'audio_files'
+chunk_directory = 'chunks'
+output_directory = 'output'
+ensure_directory_exists(audio_files_directory)
+ensure_directory_exists(chunk_directory)
+ensure_directory_exists(output_directory)
+
+# 確定完整的音訊檔案路徑
+full_audio_path = os.path.join(audio_files_directory, audio_file)
+
+# 載入和分割音訊檔案
+audio = AudioSegment.from_file(full_audio_path)
+chunk_length_ms = mins * 60 * 1000  # 分鐘轉換為毫秒
+
+
+for i in range(0, len(audio), chunk_length_ms):
+    chunk = audio[i:i+chunk_length_ms]
+    chunk_path = os.path.join(chunk_directory, f"chunk_{i//chunk_length_ms}.wav")
+    chunk.export(chunk_path, format="wav")
+
+# 載入 Whisper 模型
+model = WhisperModel(model_size, device=device, compute_type=compute_type)
+
+# 取得不含副檔名的音訊檔案名稱用作輸出檔案前綴
+audio_file_prefix = os.path.splitext(audio_file)[0]
+
+# 取得當前時間並格式化為字串，適合檔案名
+current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# 處理每個音訊片段並將結果寫入檔案
+output_path = os.path.join(output_directory, f"{audio_file_prefix}_result_{model_size}_{current_time}.txt")
+
+with open(output_path, 'w', encoding='utf-8') as file:
+    for chunk_file in sorted(os.listdir(chunk_directory)):
+        if chunk_file.endswith(".wav"):
+            chunk_path = os.path.join(chunk_directory, chunk_file)
+            logging.info(f"正在處理 {chunk_file}...")
+            segments, info = model.transcribe(chunk_path, language="zh")
+            transcription = ""
+
+            if mode == "normal":
+                transcription = ", ".join([segment.text for segment in segments])
+            elif mode == "timeline":
+                transcription = "".join(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}\n" for segment in segments)
+            elif mode == "subtitle":
+                for i, segment in enumerate(segments, 1):
+                    start_hours, start_remainder = divmod(segment.start, 3600)
+                    start_minutes, start_seconds = divmod(start_remainder, 60)
+                    end_hours, end_remainder = divmod(segment.end, 3600)
+                    end_minutes, end_seconds = divmod(end_remainder, 60)
+                    transcription += f"{i}\n{int(start_hours):02}:{int(start_minutes):02}:{start_seconds:06.3f} --> {int(end_hours):02}:{int(end_minutes):02}:{end_seconds:06.3f}\n{segment.text}\n\n"
+
+            file.write(transcription)
+            logging.info(transcription)
+    logging.info(f"已完成：{output_path}")
+
+# 腳本結束後清理 chunks 目錄
+shutil.rmtree(chunk_directory)
+
